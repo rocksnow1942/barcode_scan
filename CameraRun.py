2c8417d7962475805fa8e5abe323713edf419040
rocksnow1942 <rocksnow1942@gmail.com>
2020-11-21 01:06:38 -0800

    ON: Sat Nov 21 01:06:38 2020, FROM: mini

--- CameraRun.py
+++ CameraRun.py
@@ -5,9 +5,9 @@ import numpy as np
 # Create an array representing a 1280x720 image of
 # a cross through the center of the display. The shape of
 # the array must be of the form (height, width, color)
-a = np.zeros((720,1280 , 3), dtype=np.uint8)
-a[360, :, :] = 0xff
-a[:, 640, :] = 0xff
+a = np.zeros((480,640 , 3), dtype=np.uint8)
+a[240, :, :] = 0xff
+a[:, 320, :] = 0xff
 
 camera = picamera.PiCamera()
 camera.resolution = (1280,720)
