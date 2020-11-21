9e21b3af0af16cf4c82d4c20eb1d784dd76624ac
rocksnow1942 <rocksnow1942@gmail.com>
2020-11-21 00:58:16 -0800

    ON: Sat Nov 21 00:58:14 2020, FROM: mini

--- CameraRun.py
+++ CameraRun.py
@@ -5,7 +5,7 @@ import numpy as np
 # Create an array representing a 1280x720 image of
 # a cross through the center of the display. The shape of
 # the array must be of the form (height, width, color)
-a = np.zeros((640, 480, 3), dtype=np.uint8)
+a = np.zeros((2464, 3280, 3), dtype=np.uint8)
 a[320, :, :] = 0xff
 a[:, 240, :] = 0xff
 
@@ -14,7 +14,7 @@ camera.resolution = (3280,2464)
 camera.rotation = 90
 camera.framerate = 24
 camera.hflip = True
-camera.start_preview(fullscreen=False,window=(100,20,640,480))
+camera.start_preview() #fullscreen=False,window=(100,20,640,480)
 # Add the overlay directly into layer 3 with transparency;
 # we can omit the size parameter of add_overlay as the
 # size is the same as the camera's resolution
