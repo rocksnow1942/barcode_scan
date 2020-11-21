9c9dc2449a304a3f44450795dc08b7c90378277a
rocksnow1942 <rocksnow1942@gmail.com>
2020-11-21 01:51:19 -0800

    a

--- opencvDraw.py
+++ opencvDraw.py
@@ -9,9 +9,9 @@ layer3 = np.zeros((480, 800, 4))
 #draw a red circle on the first "layer",
 #a green rectangle on the second "layer",
 #a blue line on the third "layer"
-red_color = (0, 0, 255, 255)
-green_color = (0, 255, 0, 255)
-blue_color = (255, 0, 0, 255)
+red_color = (0, 0, 255, 125)
+green_color = (0, 255, 0, 125)
+blue_color = (255, 0, 0, 125)
 cv2.circle(layer1, (100, 100), 50, red_color, 5)
 cv2.rectangle(layer2, (175, 175), (335, 335), green_color, 5)
 cv2.line(layer3, (170, 170), (340, 340), blue_color, 5)
