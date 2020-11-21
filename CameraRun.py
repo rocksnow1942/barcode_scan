f018a228747352365963247cd5dbdb324472a93d
rocksnow1942 <rocksnow1942@gmail.com>
2020-11-21 01:11:44 -0800

    ON: Sat Nov 21 01:11:43 2020, FROM: mini

--- CameraRun.py
+++ CameraRun.py
@@ -5,16 +5,16 @@ import numpy as np
 # Create an array representing a 1280x720 image of
 # a cross through the center of the display. The shape of
 # the array must be of the form (height, width, color)
-a = np.zeros((1440,2560 , 3), dtype=np.uint8)
+a = np.zeros((2464,3280 , 3), dtype=np.uint8)
 a[240, :, :] = 0xff
 a[:, 320, :] = 0xff
 
 camera = picamera.PiCamera()
-camera.resolution = (2560,1440)
+camera.resolution = (3280,2464)
 camera.rotation = 90
 camera.framerate = 24
 camera.hflip = True
-camera.start_preview(fullscreen=False,window=(100,20,640,480)) #
+camera.start_preview(fullscreen=False,window=(100,20,615,462)) #
 # Add the overlay directly into layer 3 with transparency;
 # we can omit the size parameter of add_overlay as the
 # size is the same as the camera's resolution
