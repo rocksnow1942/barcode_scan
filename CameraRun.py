75b927505546fc797b029a98ba486ee148b1ba2f
rocksnow1942 <rocksnow1942@gmail.com>
2020-11-21 19:29:51 -0800

    a

--- CameraRun.py
+++ CameraRun.py
@@ -39,39 +39,49 @@ camera.start_preview(fullscreen=False,window=(w_x,w_y,w_w,w_h)) #
 # size is the same as the camera's resolution
 stream = BytesIO()
 
-
+count = 0
 try:
     # Wait indefinitely until the user terminates the script
-    while True:
+    while True:        
+        time.sleep(1)
+        camera.remove_overlay(o)
+        pad = Image.new('RGBA',(800,480))
+        count += 1
+        padDraw = ImageDraw.Draw(pad)
+        outline = (0,255,0,180) if count%2 else ((0,0,255,180))
+        padDraw.rectangle([150,110,170,130],fill=(0,0,0,0),outline=outline)
+        # padDraw.rectangle([x*w_w//c_w,y*w_w//c_w,(x+w)*w_w//c_w,(y+h)*w_w//c_w],fill=(0,0,0,0),outline=(0,255,0,180))
+        o = camera.add_overlay(pad.tobytes(),size=pad.size, layer=3)
         
-        # capture and detect
-        stream.seek(0)
-        camera.capture(stream,format='jpeg',) #resize=(c_w,c_h)
-        stream.seek(0)
-        img = Image.open(stream)
-        # img = ImageOps.mirror(img)
-        
-        code = decode(img)
-        if code:
-            if o:
-                camera.remove_overlay(o)
-            code = code[0]
-            print(code.data.decode())
-            xy = [ (i.x*w_w//c_w +w_x ,i.y*w_w//c_w + w_y) for i in code.polygon]
-            # 
-            # x,y = code.rect.left,code.rect.top
-            # w,h = code.rect.width,code.rect.height
-            pad = Image.new('RGBA',(800,480))
-
-            padDraw = ImageDraw.Draw(pad)
-
-            padDraw.polygon(xy,fill=(0,0,0,0),outline=(0,255,0,180))
-            # padDraw.rectangle([x*w_w//c_w,y*w_w//c_w,(x+w)*w_w//c_w,(y+h)*w_w//c_w],fill=(0,0,0,0),outline=(0,255,0,180))
-            o = camera.add_overlay(pad.tobytes(),size=pad.size, layer=3)
-        else:
-            if o:
-                camera.remove_overlay(o)
-                o = None
+        # # capture and detect
+        # stream.seek(0)
+        # camera.capture(stream,format='jpeg',) #resize=(c_w,c_h)
+        # stream.seek(0)
+        # img = Image.open(stream)
+        # # img = ImageOps.mirror(img)
+        # 
+        # code = decode(img)
+        # if code:
+        #     if o:
+        #         camera.remove_overlay(o)
+        #     code = code[0]
+        #     print(code.data.decode())
+        #     xy = [ (i.x*w_w//c_w +w_x ,i.y*w_w//c_w + w_y) for i in code.polygon]
+        #     # 
+        #     # x,y = code.rect.left,code.rect.top
+        #     # w,h = code.rect.width,code.rect.height
+        #     pad = Image.new('RGBA',(800,480))
+        # 
+        #     padDraw = ImageDraw.Draw(pad)
+        # 
+        #     padDraw.polygon(xy,fill=(0,0,0,0),outline=(0,255,0,180))
+        #     # padDraw.rectangle([x*w_w//c_w,y*w_w//c_w,(x+w)*w_w//c_w,(y+h)*w_w//c_w],fill=(0,0,0,0),outline=(0,255,0,180))
+        #     o = camera.add_overlay(pad.tobytes(),size=pad.size, layer=3)
+        # else:
+        #     if o:
+        #         camera.remove_overlay(o)
+        #         o = None
 finally:
-    camera.remove_overlay(o)
-    pass
+    if o:
+        camera.remove_overlay(o)
+    
